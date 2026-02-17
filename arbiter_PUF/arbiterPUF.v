19f146f5a65664aad9cc0fa8a877e24f03a36d56
VanGaurd45 <anikrohit45264@gmail.com>
2026-02-17 21:31:38 +0530

    Update arbiterPUF.v

--- arbiter_PUF/arbiterPUF.v
+++ arbiter_PUF/arbiterPUF.v
@@ -1,54 +1,40 @@
-`timescale 1ns/1ps
-module arbiter #(parameter N = 128) (
-  input in,reset,[N-1:0]sel,
-  output out
-);
-  wire [N-1:0] m;
-  wire [N-1:0] n;
+`timescale 1ns / 1ps
 
-  mux_block block0 (in,~in,sel[0],m[0],n[0]);
+module arbiter #(parameter N = 4)(input in, input [N-1:0] sel, input reset, output out );
+    
+    wire [N-1:0]up;
+    wire [N-1:0]down;
+    
+    mux_block block0 (in,~in,sel[0],up[0],down[0]);
+    generate 
+        genvar i;
+        for(i = 0; i < N-1; i=i+1) begin: mux_blocks
+            mux_block iblock (up[i],down[i],sel[i+1],up[i+1],down[i+1]);
+        end
+    endgenerate
+    
+    d_ff D (up[N-1],down[N-1],reset,out);
+    
+endmodule
 
-  generate
-    genvar i;
-    for (i = 0; i < N - 1; i = i + 1) begin: mux_block
-      mux_block block(
-        m[i],
-        n[i],
-        sel[i],
-        m[i+1],
-        n[i+1]
-      );
+module d_ff(input d, input clk, input reset, output reg Q);
+
+    always @(posedge clk or posedge reset) begin
+        if(reset)   Q<=1'b0;
+        else Q<=d;
     end
-  endgenerate
-  d_latch sr(n[N-1],m[N-1],reset,out);
-
-endmodule  
-
-module d_latch(
-  input d_in, clk, reset,
-  output reg q_out
-);
-  always @( posedge clk or posedge reset) begin
-    if(reset)
-      q_out <= 1'b0;
-    else if (!reset)
-      q_out <= d_in;
-  end
+    
 endmodule
 
-module mux_block(
-  input in_up,in_down,sel,
-  output out_up,out_down
-);
-  
-  mux2x1 #0.1 mux_up(in_up,in_down,sel,out_up);
-  mux2x1 #0.2 mux_down(in_down,in_up,sel,out_down);
-  
+module mux2X1(input i0, input i1, input sel, output out);
+
+    assign out = sel ? i1 : i0;
+    
 endmodule
 
-module mux2x1(
-  input in_0, in_1,sel,
-  output out
-);
-  assign out = sel ? in_1 : in_0;
+module mux_block(input in_up, input in_down, input sel, output out_up, output out_down);
+
+    mux2X1 #0.1 mux_up (in_up,in_down,sel,out_up);
+    mux2X1 #0.2 mux_down (in_down,in_up,sel,out_down);
+    
 endmodule
